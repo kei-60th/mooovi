d586c607e0e06d2ceedff10bb72c573dd9197e2c
kei-60th <kei1005@i.softbank.jp>
2020-06-11 23:46:13 +0900

    すべてのページから映画の情報を取得できるようにする

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,17 +2,22 @@ class Scraping
   def self.movie_urls
     links = []
     agent = Mechanize.new
-    current_page = agent.get("http://review-movie.herokuapp.com/")
-    elements = current_page.search('.entry-title a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
+    next_url = ""
+
+    while true
+      current_page = agent.get("http://review-movie.herokuapp.com/" + next_url)
+      elements = current_page.search('.entry-title a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('.pagination .next a')
+      break unless next_link
+      next_url = next_link.get_attribute('href')
+
     end
-
-    #movie_urlsメソッドではまず、20件分の映画の個別ページのリンクを取得する
-
     links.each do |link|
-      get_product('http://review-movie.herokuapp.com/' + link)
-      #取得したリンクURLにMovieReviewを結合して、get_productメソッドに引数として渡す
+      get_product('http://review-movie.herokuapp.com' + link)
     end
   end
 
