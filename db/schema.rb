29ae43b27950c2ff536400bbefd3ec1e9d712411
kei-60th <kei1005@i.softbank.jp>
2020-06-11 23:58:53 +0900

    マイグレーションファイルの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_142322) do
+ActiveRecord::Schema.define(version: 2020_06_11_145326) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.text "detail"
+    t.string "open_date"
   end
 
 end
